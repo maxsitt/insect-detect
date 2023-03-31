066587c548339ba1d40c2b46e4ee6aa4bc407d9a
maxsitt <87697856+maxsitt@users.noreply.github.com>
2023-03-31 21:53:54 +0200

    Update yolo_tracker_preview.py
    Use 1080p as default resolution (higher fps). Harmonize with other scripts.

--- yolo_tracker_preview.py
+++ yolo_tracker_preview.py
@@ -7,13 +7,14 @@ License:  GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
 
 This Python script does the following:
 - run a custom YOLO object detection model (.blob format) on-device (Luxonis OAK)
-- use 4K frames downscaled to full FOV LQ frames (e.g. 320x320) as model input
-- use an object tracker (Intel DL Streamer) to track detected objects and set unique tracking IDs
-- show a preview of 4K frames downscaled to full FOV LQ frames (e.g. 320x320) + model/tracker output
+  -> inference on downscaled LQ frames (e.g. 320x320 px)
+- use an object tracker to track detected objects and assign unique tracking IDs (on-device)
+- show downscaled LQ frames + model/tracker output (bounding box, label, confidence,
+  tracking ID, tracking status) + fps in a new window (e.g. via X11 forwarding)
 - optional argument:
-  "-log" print available Raspberry Pi memory (MB) and RPi CPU utilization (percent) to console
+  "-log" print available Raspberry Pi memory (MB) and RPi CPU utilization (%) to console
 
-compiled with open source scripts available at https://github.com/luxonis
+based on open source scripts available at https://github.com/luxonis
 '''
 
 import argparse
@@ -25,10 +26,10 @@ import cv2
 import depthai as dai
 import numpy as np
 
-# Define optional arguments
+# Define optional argument
 parser = argparse.ArgumentParser()
 parser.add_argument("-log", "--print_log", action="store_true",
-    help="print RPi available memory (MB) + CPU utilization (percent)")
+    help="print RPi available memory (MB) + CPU utilization (%)")
 args = parser.parse_args()
 
 if args.print_log:
@@ -38,7 +39,7 @@ if args.print_log:
 MODEL_PATH = Path("insect-detect/models/yolov5n_320_openvino_2022.1_4shave.blob")
 CONFIG_PATH = Path("insect-detect/models/json/yolov5_v7_320.json")
 
-# Extract detection model metadata from config JSON
+# Get detection model metadata from config JSON
 with CONFIG_PATH.open(encoding="utf-8") as f:
     config = json.load(f)
 nn_config = config.get("nn_config", {})
@@ -58,11 +59,12 @@ pipeline = dai.Pipeline()
 # Create and configure camera node
 cam_rgb = pipeline.create(dai.node.ColorCamera)
 #cam_rgb.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
-cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
+cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
 cam_rgb.setPreviewSize(320, 320) # downscaled LQ frames for model input
-cam_rgb.setInterleaved(False)
-cam_rgb.setPreviewKeepAspectRatio(False) # squash full FOV frames to square
-cam_rgb.setFps(41) # frames per second available for focus/exposure/model input
+cam_rgb.setPreviewKeepAspectRatio(False) # "squeeze" frames (16:9) to square (1:1)
+cam_rgb.setInterleaved(False) # planar layout
+cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+cam_rgb.setFps(47) # frames per second available for focus/exposure/model input
 
 # Create detection network node and define input
 nn = pipeline.create(dai.node.YoloDetectionNetwork)
@@ -82,20 +84,19 @@ nn.setNumInferenceThreads(2)
 # Create and configure object tracker node and define inputs + outputs
 tracker = pipeline.create(dai.node.ObjectTracker)
 tracker.setTrackerType(dai.TrackerType.ZERO_TERM_IMAGELESS)
-# use short term tracker if fps < ~30 for better tracking performance
-#tracker.setTrackerType(dai.TrackerType.SHORT_TERM_IMAGELESS)
+#tracker.setTrackerType(dai.TrackerType.SHORT_TERM_IMAGELESS) # better for low fps
 tracker.setTrackerIdAssignmentPolicy(dai.TrackerIdAssignmentPolicy.UNIQUE_ID)
 nn.passthrough.link(tracker.inputTrackerFrame)
 nn.passthrough.link(tracker.inputDetectionFrame)
 nn.out.link(tracker.inputDetections)
 
-xout_tracker = pipeline.create(dai.node.XLinkOut)
-xout_tracker.setStreamName("track")
-tracker.out.link(xout_tracker.input)
-
 xout_rgb = pipeline.create(dai.node.XLinkOut)
 xout_rgb.setStreamName("frame")
 tracker.passthroughTrackerFrame.link(xout_rgb.input)
+
+xout_tracker = pipeline.create(dai.node.XLinkOut)
+xout_tracker.setStreamName("track")
+tracker.out.link(xout_tracker.input)
 
 # Define function to convert relative bounding box coordinates (0-1) to pixel coordinates
 def frame_norm(frame, bbox):
@@ -104,34 +105,33 @@ def frame_norm(frame, bbox):
     norm_vals[::2] = frame.shape[1]
     return (np.clip(np.array(bbox), 0, 1) * norm_vals).astype(int)
 
-# Connect to OAK device and start pipeline
+# Connect to OAK device and start pipeline in USB2 mode
 with dai.Device(pipeline, usb2Mode=True) as device:
 
-    # Create output queues to get the frames and detections from the outputs defined above
+    # Create output queues to get the frames and tracklets + detections from the outputs defined above
     q_frame = device.getOutputQueue(name="frame", maxSize=4, blocking=False)
     q_track = device.getOutputQueue(name="track", maxSize=4, blocking=False)
 
-    # Create start_time and counter variables to measure fps of the detection model + tracker
+    # Create start_time and counter variable to measure fps
     start_time = time.monotonic()
     counter = 0
 
-    # Get LQ preview frames and tracker output (detections + t.ID) and show in window
+    # Get LQ frames + tracker output (passthrough detections) and show in new window
     while True:
         if args.print_log:
             print(f"Available RPi memory: {round(psutil.virtual_memory().available / 1048576)} MB")
-            print(f"RPi CPU utilization:  {psutil.cpu_percent(interval=None)}%")
-            print("\n")
+            print(f"RPi CPU utilization:  {psutil.cpu_percent(interval=None)}%\n")
 
         frame = q_frame.get().getCvFrame()
         track_out = q_track.get()
 
         if track_out is not None:
-            tracklets_data = track_out.tracklets
+            tracks = track_out.tracklets
             counter += 1
-            fps = counter / (time.monotonic() - start_time)
+            fps = round(counter / (time.monotonic() - start_time), 2)
 
         if frame is not None:
-            for t in tracklets_data:
+            for t in tracks:
                 roi = t.roi.denormalize(frame.shape[1], frame.shape[0])
                 x1 = int(roi.topLeft().x)
                 y1 = int(roi.topLeft().y)
@@ -148,16 +148,16 @@ with dai.Device(pipeline, usb2Mode=True) as device:
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
                 cv2.putText(frame, t.status.name, (bbox[0], bbox[3] + 50),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2) # model output
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 130), 1) # tracker output
+                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2) # model bbox
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 130), 1) # tracker bbox
 
-            cv2.putText(frame, f"fps: {round(fps, 2)}", (4, frame.shape[0] - 10),
+            cv2.putText(frame, f"fps: {fps}", (4, frame.shape[0] - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-
             cv2.imshow("tracker_preview", frame)
+            #print(f"fps: {fps}")
             # streaming the frames via SSH (X11 forwarding) will slow down fps
-            # comment out 'cv2.imshow()' and print fps to console for "true" fps
-            #print(f"fps: {round(fps, 2)}")
+            # comment out "cv2.imshow()" and print fps to console for true fps
 
+        # Stop script and close window by pressing "Q"
         if cv2.waitKey(1) == ord("q"):
             break

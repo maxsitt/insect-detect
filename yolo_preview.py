135e75f8f4d0b2e6e5764f1c2c5f2eecf052fd4f
maxsitt <87697856+maxsitt@users.noreply.github.com>
2023-03-31 21:53:13 +0200

    Update yolo_preview.py
    Use 1080p as default resolution (higher fps). Harmonize with other scripts.

--- yolo_preview.py
+++ yolo_preview.py
@@ -7,12 +7,13 @@ License:  GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
 
 This Python script does the following:
 - run a custom YOLO object detection model (.blob format) on-device (Luxonis OAK)
-- use 4K frames downscaled to full FOV LQ frames (e.g. 320x320) as model input
-- show a preview of 4K frames downscaled to full FOV LQ frames (e.g. 320x320) + model output
+  -> inference on downscaled LQ frames (e.g. 320x320 px)
+- show downscaled LQ frames + model output (bounding box, label, confidence) + fps
+  in a new window (e.g. via X11 forwarding)
 - optional argument:
-  "-log" print available Raspberry Pi memory (MB) and RPi CPU utilization (percent) to console
+  "-log" print available Raspberry Pi memory (MB) and RPi CPU utilization (%) to console
 
-compiled with open source scripts available at https://github.com/luxonis
+based on open source scripts available at https://github.com/luxonis
 '''
 
 import argparse
@@ -24,10 +25,10 @@ import cv2
 import depthai as dai
 import numpy as np
 
-# Define optional arguments
+# Define optional argument
 parser = argparse.ArgumentParser()
 parser.add_argument("-log", "--print_log", action="store_true",
-    help="print RPi available memory (MB) + CPU utilization (percent)")
+    help="print RPi available memory (MB) + CPU utilization (%)")
 args = parser.parse_args()
 
 if args.print_log:
@@ -37,7 +38,7 @@ if args.print_log:
 MODEL_PATH = Path("insect-detect/models/yolov5n_320_openvino_2022.1_4shave.blob")
 CONFIG_PATH = Path("insect-detect/models/json/yolov5_v7_320.json")
 
-# Extract detection model metadata from config JSON
+# Get detection model metadata from config JSON
 with CONFIG_PATH.open(encoding="utf-8") as f:
     config = json.load(f)
 nn_config = config.get("nn_config", {})
@@ -57,24 +58,25 @@ pipeline = dai.Pipeline()
 # Create and configure camera node
 cam_rgb = pipeline.create(dai.node.ColorCamera)
 #cam_rgb.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
-cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
+cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
 cam_rgb.setPreviewSize(320, 320) # downscaled LQ frames for model input
-cam_rgb.setInterleaved(False)
-cam_rgb.setPreviewKeepAspectRatio(False) # squash full FOV frames to square
-cam_rgb.setFps(41) # frames per second available for focus/exposure/model input
+cam_rgb.setPreviewKeepAspectRatio(False) # "squeeze" frames (16:9) to square (1:1)
+cam_rgb.setInterleaved(False) # planar layout
+cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+cam_rgb.setFps(49) # frames per second available for focus/exposure/model input
 
 # Create detection network node and define input + outputs
 nn = pipeline.create(dai.node.YoloDetectionNetwork)
 cam_rgb.preview.link(nn.input) # downscaled LQ frames as model input
 nn.input.setBlocking(False)
 
-xout_nn = pipeline.create(dai.node.XLinkOut)
-xout_nn.setStreamName("nn")
-nn.out.link(xout_nn.input)
-
 xout_rgb = pipeline.create(dai.node.XLinkOut)
 xout_rgb.setStreamName("frame")
 nn.passthrough.link(xout_rgb.input)
+
+xout_nn = pipeline.create(dai.node.XLinkOut)
+xout_nn.setStreamName("nn")
+nn.out.link(xout_nn.input)
 
 # Set detection model specific settings
 nn.setBlobPath(MODEL_PATH)
@@ -93,23 +95,22 @@ def frame_norm(frame, bbox):
     norm_vals[::2] = frame.shape[1]
     return (np.clip(np.array(bbox), 0, 1) * norm_vals).astype(int)
 
-# Connect to OAK device and start pipeline
+# Connect to OAK device and start pipeline in USB2 mode
 with dai.Device(pipeline, usb2Mode=True) as device:
 
     # Create output queues to get the frames and detections from the outputs defined above
     q_frame = device.getOutputQueue(name="frame", maxSize=4, blocking=False)
     q_nn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
 
-    # Create start_time and counter variables to measure fps of the detection model
+    # Create start_time and counter variable to measure fps
     start_time = time.monotonic()
     counter = 0
 
-    # Get LQ preview frames and model output (detections) and show in window
+    # Get LQ frames + model output (detections) and show in new window
     while True:
         if args.print_log:
             print(f"Available RPi memory: {round(psutil.virtual_memory().available / 1048576)} MB")
-            print(f"RPi CPU utilization:  {psutil.cpu_percent(interval=None)}%")
-            print("\n")
+            print(f"RPi CPU utilization:  {psutil.cpu_percent(interval=None)}%\n")
 
         frame = q_frame.get().getCvFrame()
         nn_out = q_nn.get()
@@ -117,7 +118,7 @@ with dai.Device(pipeline, usb2Mode=True) as device:
         if nn_out is not None:
             dets = nn_out.detections
             counter += 1
-            fps = counter / (time.monotonic() - start_time)
+            fps = round(counter / (time.monotonic() - start_time), 2)
 
         if frame is not None:
             for detection in dets:
@@ -129,13 +130,13 @@ with dai.Device(pipeline, usb2Mode=True) as device:
                             cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
                 cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2)
 
-            cv2.putText(frame, f"fps: {round(fps, 2)}", (4, frame.shape[0] - 10),
+            cv2.putText(frame, f"fps: {fps}", (4, frame.shape[0] - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-
             cv2.imshow("yolo_preview", frame)
+            #print(f"fps: {fps}")
             # streaming the frames via SSH (X11 forwarding) will slow down fps
-            # comment out 'cv2.imshow()' and print fps to console for "true" fps
-            #print(f"fps: {round(fps, 2)}")
+            # comment out "cv2.imshow()" and print fps to console for true fps
 
+        # Stop script and close window by pressing "Q"
         if cv2.waitKey(1) == ord("q"):
             break

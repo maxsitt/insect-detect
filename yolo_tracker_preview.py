5b62dfb0d645df77f4c18e0fe78ce3ba1f6613b1
maxsitt <87697856+maxsitt@users.noreply.github.com>
2023-05-08 20:34:55 +0200

    Update yolo_tracker_preview.py
    Optimize reading procedure from the output queues. Print logs at readable speed by using a BackgroundScheduler. Add OAK info to logs.

--- yolo_tracker_preview.py
+++ yolo_tracker_preview.py
@@ -12,7 +12,8 @@ This Python script does the following:
 - show downscaled LQ frames + model/tracker output (bounding box, label, confidence,
   tracking ID, tracking status) + fps in a new window (e.g. via X11 forwarding)
 - optional argument:
-  "-log" print available Raspberry Pi memory (MB) and RPi CPU utilization (%) to console
+  "-log" print available Raspberry Pi memory, RPi CPU utilization + temperature,
+         OAK memory + CPU usage and OAK chip temperature to console
 
 based on open source scripts available at https://github.com/luxonis
 '''
@@ -28,12 +29,15 @@ import numpy as np
 
 # Define optional argument
 parser = argparse.ArgumentParser()
-parser.add_argument("-log", "--print_log", action="store_true",
-    help="print RPi available memory (MB) + CPU utilization (%)")
+parser.add_argument("-log", "--print_logs", action="store_true",
+    help="print RPi available memory, RPi CPU utilization + temperature, \
+          OAK memory + CPU usage and OAK chip temperature to console")
 args = parser.parse_args()
 
-if args.print_log:
+if args.print_logs:
     import psutil
+    from apscheduler.schedulers.background import BackgroundScheduler
+    from gpiozero import CPUTemperature
 
 # Set file paths to the detection model and config JSON
 MODEL_PATH = Path("insect-detect/models/yolov5n_320_openvino_2022.1_4shave.blob")
@@ -98,15 +102,29 @@ xout_tracker = pipeline.create(dai.node.XLinkOut)
 xout_tracker.setStreamName("track")
 tracker.out.link(xout_tracker.input)
 
-# Define function to convert relative bounding box coordinates (0-1) to pixel coordinates
+# Define functions
 def frame_norm(frame, bbox):
     """Convert relative bounding box coordinates (0-1) to pixel coordinates."""
     norm_vals = np.full(len(bbox), frame.shape[0])
     norm_vals[::2] = frame.shape[1]
     return (np.clip(np.array(bbox), 0, 1) * norm_vals).astype(int)
 
+def print_logs():
+    """Print Raspberry Pi info to console."""
+    print(f"\nAvailable RPi memory: {round(psutil.virtual_memory().available / 1048576)} MB")
+    print(f"RPi CPU utilization:  {round(psutil.cpu_percent(interval=None))} %")
+    print(f"RPi CPU temperature:  {round(CPUTemperature().temperature)} °C\n")
+
 # Connect to OAK device and start pipeline in USB2 mode
 with dai.Device(pipeline, usb2Mode=True) as device:
+
+    # Print RPi + OAK info to console every second
+    if args.print_logs:
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(print_logs, "interval", seconds=1, id="log")
+        scheduler.start()
+        device.setLogLevel(dai.LogLevel.INFO)
+        device.setLogOutputLevel(dai.LogLevel.INFO)
 
     # Create output queues to get the frames and tracklets + detections from the outputs defined above
     q_frame = device.getOutputQueue(name="frame", maxSize=4, blocking=False)
@@ -118,45 +136,40 @@ with dai.Device(pipeline, usb2Mode=True) as device:
 
     # Get LQ frames + tracker output (passthrough detections) and show in new window
     while True:
-        if args.print_log:
-            print(f"Available RPi memory: {round(psutil.virtual_memory().available / 1048576)} MB")
-            print(f"RPi CPU utilization:  {psutil.cpu_percent(interval=None)}%\n")
+        if q_frame.has():
+            frame = q_frame.get().getCvFrame()
 
-        frame = q_frame.get().getCvFrame()
-        track_out = q_track.get()
+            if q_track.has():
+                tracks = q_track.get().tracklets
+                counter += 1
+                fps = round(counter / (time.monotonic() - start_time), 2)
 
-        if track_out is not None:
-            tracks = track_out.tracklets
-            counter += 1
-            fps = round(counter / (time.monotonic() - start_time), 2)
+                for track in tracks:
+                    roi = track.roi.denormalize(frame.shape[1], frame.shape[0])
+                    x1 = int(roi.topLeft().x)
+                    y1 = int(roi.topLeft().y)
+                    x2 = int(roi.bottomRight().x)
+                    y2 = int(roi.bottomRight().y)
 
-        if frame is not None:
-            for t in tracks:
-                roi = t.roi.denormalize(frame.shape[1], frame.shape[0])
-                x1 = int(roi.topLeft().x)
-                y1 = int(roi.topLeft().y)
-                x2 = int(roi.bottomRight().x)
-                y2 = int(roi.bottomRight().y)
+                    bbox = frame_norm(frame, (track.srcImgDetection.xmin, track.srcImgDetection.ymin,
+                                              track.srcImgDetection.xmax, track.srcImgDetection.ymax))
+                    cv2.putText(frame, labels[track.srcImgDetection.label], (bbox[0], bbox[3] + 13),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
+                    cv2.putText(frame, f"{round(track.srcImgDetection.confidence, 2)}", (bbox[0], bbox[3] + 25),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
+                    cv2.putText(frame, f"ID:{track.id}", (bbox[0], bbox[3] + 40),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(frame, track.status.name, (bbox[0], bbox[3] + 50),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+                    cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2) # model bbox
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 130), 1) # tracker bbox
 
-                bbox = frame_norm(frame, (t.srcImgDetection.xmin, t.srcImgDetection.ymin,
-                                          t.srcImgDetection.xmax, t.srcImgDetection.ymax))
-                cv2.putText(frame, labels[t.srcImgDetection.label], (bbox[0], bbox[3] + 13),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
-                cv2.putText(frame, f"{round(t.srcImgDetection.confidence, 2)}", (bbox[0], bbox[3] + 25),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
-                cv2.putText(frame, f"ID:{t.id}", (bbox[0], bbox[3] + 40),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-                cv2.putText(frame, t.status.name, (bbox[0], bbox[3] + 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2) # model bbox
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 130), 1) # tracker bbox
-
-            cv2.putText(frame, f"fps: {fps}", (4, frame.shape[0] - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-            cv2.imshow("tracker_preview", frame)
-            #print(f"fps: {fps}")
-            # streaming the frames via SSH (X11 forwarding) will slow down fps
-            # comment out "cv2.imshow()" and print fps to console for true fps
+                cv2.putText(frame, f"fps: {fps}", (4, frame.shape[0] - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+                cv2.imshow("tracker_preview", frame)
+                #print(f"fps: {fps}")
+                # streaming the frames via SSH (X11 forwarding) will slow down fps
+                # comment out "cv2.imshow()" and print fps to console for true fps
 
         # Stop script and close window by pressing "Q"
         if cv2.waitKey(1) == ord("q"):

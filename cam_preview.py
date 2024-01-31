a8f35a376bf5e18c13a32ec360bd198611a9a806
maxsitt <87697856+maxsitt@users.noreply.github.com>
2024-01-31 18:40:14 +0100

    Update cam_preview.py
    Add option to show bigger camera preview (640x640 px).

--- cam_preview.py
+++ cam_preview.py
@@ -6,28 +6,41 @@ Website:  https://maxsitt.github.io/insect-detect-docs/
 License:  GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
 
 This Python script does the following:
-- show downscaled LQ frames (e.g. 320x320 px) + fps in a new window (e.g. via X11 forwarding)
+- show downscaled LQ frames + fps in a new window (e.g. via X11 forwarding)
+- optional argument:
+  "-big" show a bigger (640x640 px) preview window (default: 320x320 px)
+         -> decreases frame rate to ~3 fps (default: ~11 fps)
 
 based on open source scripts available at https://github.com/luxonis
 '''
 
+import argparse
 import time
 
 import cv2
 import depthai as dai
 
+# Define optional argument
+parser = argparse.ArgumentParser()
+parser.add_argument("-big", "--big_preview", action="store_true",
+    help="show a bigger (640x640 px) preview window; default = 320x320 px")
+args = parser.parse_args()
+
 # Create depthai pipeline
 pipeline = dai.Pipeline()
 
-# Create and configure camera node and define output
+# Create and configure color camera node and define output
 cam_rgb = pipeline.create(dai.node.ColorCamera)
 #cam_rgb.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
 cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-cam_rgb.setPreviewSize(320, 320) # downscaled LQ frames
-cam_rgb.setPreviewKeepAspectRatio(False) # "squeeze" frames (16:9) to square (1:1)
+if not args.big_preview:
+    cam_rgb.setPreviewSize(320, 320) # downscaled LQ frames
+else:
+    cam_rgb.setPreviewSize(640, 640)
+cam_rgb.setPreviewKeepAspectRatio(False) # stretch frames (16:9) to square (1:1)
 cam_rgb.setInterleaved(False) # planar layout
 cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-cam_rgb.setFps(25) # frames per second available for focus/exposure
+cam_rgb.setFps(25) # frames per second available for auto focus/exposure
 
 xout_rgb = pipeline.create(dai.node.XLinkOut)
 xout_rgb.setStreamName("frame")
@@ -43,9 +56,10 @@ with dai.Device(pipeline, maxUsbSpeed=dai.UsbSpeed.HIGH) as device:
     start_time = time.monotonic()
     counter = 0
 
-    # Get LQ frames and show in new window
+    # Get LQ frames and show in new window together with fps
     while True:
         frame = q_frame.get().getCvFrame()
+
         counter += 1
         fps = round(counter / (time.monotonic() - start_time), 2)
 

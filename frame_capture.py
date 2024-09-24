0a41a99d9df375867adde2eb67a39e11e54578cb
maxsitt <87697856+maxsitt@users.noreply.github.com>
2024-09-24 19:38:46 +0200

    Refactor frame_capture.py
    Use MJPEG encoder and still capture to enable 12MP resolution. Expose configuration of ISP settings to potentially increase image quality depending on the use case. Wait for 3 seconds to adjust auto focus/exposure. Optimize code structure. Remove still_capture.py.

--- frame_capture.py
+++ frame_capture.py
@@ -7,18 +7,21 @@ License:  GNU GPLv3 (https://choosealicense.com/licenses/gpl-3.0/)
 Author:   Maximilian Sittinger (https://github.com/maxsitt)
 Docs:     https://maxsitt.github.io/insect-detect-docs/
 
-- save HQ frames (default: 1920x1080 px) to .jpg at the
-  specified capture frequency (default: 1 s)
-  -> stop recording early if free disk space drops below threshold
+- save HQ frames to .jpg at the specified capture frequency (default: 1 second)
+  -> stop recording early if free disk space drops below threshold (default: 100 MB)
 - optional arguments:
-  '-min' set recording time in minutes (default: 2 [min])
+  '-min' set recording time in minutes (default: 40)
          -> e.g. '-min 5' for 5 min recording time
-  '-4k'  set camera resolution to 4K (3840x2160 px) (default: 1080p)
-  '-lq'  additionally save downscaled LQ frames (e.g. 320x320 px)
+  '-res' set camera resolution for HQ frames
+         -> '-res 1080p' for 1920x1080 px
+         -> '-res 4k' for 3840x2160 px (default - cropped from 12MP)
+         -> '-res 12mp' for 4032x3040 px
   '-af'  set auto focus range in cm (min - max distance to camera)
          -> e.g. '-af 14 20' to restrict auto focus range to 14-20 cm
   '-mf'  set manual focus position in cm (distance to camera)
          -> e.g. '-mf 14' to set manual focus position to 14 cm
+  '-lq'  additionally save downscaled + stretched LQ frames (default: 320x320 px)
+         -> FOV from 4K/1080p resolution, full FOV from 12MP is not preserved
 
 based on open source scripts available at https://github.com/luxonis
 """
@@ -38,31 +41,37 @@ from utils.oak_cam import convert_cm_lens_position
 # Define optional arguments
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-parser.add_argument("-min", "--min_rec_time", type=int, choices=range(1, 721), default=2,
-    help="Set recording time in minutes (default: 2 [min]).", metavar="1-720")
-parser.add_argument("-4k", "--four_k_resolution", action="store_true",
-    help="Set camera resolution to 4K (3840x2160 px) (default: 1080p).")
-parser.add_argument("-lq", "--save_lq_frames", action="store_true",
-    help="Additionally save downscaled LQ frames (320x320 px).")
+parser.add_argument("-min", "--min_rec_time", type=int, choices=range(1, 721), default=40,
+    help="Set recording time in minutes (default: 40).", metavar="1-720")
+parser.add_argument("-res", "--resolution", choices=["1080p", "4k", "12mp"], default="4k", type=str,
+    help="Set camera resolution (default: 4k).")
 group.add_argument("-af", "--af_range", nargs=2, type=int,
     help="Set auto focus range in cm (min - max distance to camera).", metavar=("CM_MIN", "CM_MAX"))
 group.add_argument("-mf", "--manual_focus", type=int,
     help="Set manual focus position in cm (distance to camera).", metavar="CM")
+parser.add_argument("-lq", "--save_lq_frames", action="store_true",
+    help="Additionally save downscaled LQ frames.")
 args = parser.parse_args()
 
-# Set threshold value required to start and continue a recording
-MIN_DISKSPACE = 100  # minimum free disk space (MB) (default: 100 MB)
+# Set minimum free disk space required to start and continue a recording (default: 100 MB)
+MIN_DISKSPACE = 100
 
 # Set capture frequency (default: 1 second)
 # -> wait for specified amount of seconds between saving HQ frames
-# 'CAPTURE_FREQ = 0.8' (0.2 for 4K) saves ~58 frames per minute to .jpg
-CAPTURE_FREQ = 0.8 if not args.four_k_resolution else 0.2
+# 'CAPTURE_FREQ = 0' tries to save HQ frames at the maximum possible frame rate
+CAPTURE_FREQ = 1
 
-# Set recording time (default: 2 minutes)
+# Set camera frame rate (default: 25 FPS)
+FPS = 25
+
+# Set recording time (default: 40 minutes)
 REC_TIME = args.min_rec_time * 60
 
 # Set logging level and format
 logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+# Get free disk space (MB)
+disk_free = round(psutil.disk_usage("/").free / 1048576)
 
 # Create directory per day (date) and recording interval (date_time) to save HQ frames (+ LQ frames)
 rec_start_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -74,76 +83,102 @@ if args.save_lq_frames:
 # Create depthai pipeline
 pipeline = dai.Pipeline()
 
-# Create and configure color camera node and define output(s)
+# Create and configure color camera node
 cam_rgb = pipeline.create(dai.node.ColorCamera)
 #cam_rgb.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)  # rotate image 180°
-cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
-if not args.four_k_resolution:
-    cam_rgb.setIspScale(1, 2)  # downscale 4K to 1080p resolution -> HQ frames
-cam_rgb.setInterleaved(False)  # planar layout
-cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-cam_rgb.setFps(25)  # frames per second available for auto focus/exposure
+if args.resolution == "12mp":
+    cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_12_MP)  # 4032x3040 px
+else:
+    cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)    # 3840x2160 px
+    if args.resolution == "1080p":
+        cam_rgb.setIspScale(1, 2)  # downscale 4K to 1080p resolution            # 1920x1080 px
 if args.save_lq_frames:
     cam_rgb.setPreviewSize(320, 320)  # downscale frames -> LQ frames
     cam_rgb.setPreviewKeepAspectRatio(False)  # stretch frames (16:9) to square (1:1)
-
-xout_rgb = pipeline.create(dai.node.XLinkOut)
-xout_rgb.setStreamName("frame")
-cam_rgb.video.link(xout_rgb.input)  # HQ frames
+cam_rgb.setInterleaved(False)  # planar layout
+cam_rgb.setFps(FPS)  # frames per second available for auto focus/exposure
 
 if args.save_lq_frames:
+    # Create output node for LQ frames
     xout_lq = pipeline.create(dai.node.XLinkOut)
     xout_lq.setStreamName("frame_lq")
-    cam_rgb.preview.link(xout_lq.input)  # LQ frames
+    cam_rgb.preview.link(xout_lq.input)
 
 if args.af_range:
     # Convert cm to lens position values and set auto focus range
     lens_pos_min, lens_pos_max = convert_cm_lens_position((args.af_range[1], args.af_range[0]))
     cam_rgb.initialControl.setAutoFocusLensRange(lens_pos_min, lens_pos_max)
-
-if args.manual_focus:
+elif args.manual_focus:
     # Convert cm to lens position value and set manual focus position
     lens_pos = convert_cm_lens_position(args.manual_focus)
     cam_rgb.initialControl.setManualFocus(lens_pos)
 
+# Configure ISP settings (default: 1, range: 0-4)
+# -> setting Sharpness and LumaDenoise to 0 can reduce artifacts in some cases
+cam_rgb.initialControl.setSharpness(1)
+cam_rgb.initialControl.setLumaDenoise(1)
+cam_rgb.initialControl.setChromaDenoise(1)
+
+# Create and configure video encoder node and define input + output
+video_enc = pipeline.create(dai.node.VideoEncoder)
+video_enc.setDefaultProfilePreset(1, dai.VideoEncoderProperties.Profile.MJPEG)
+video_enc.setQuality(80)  # JPEG quality (0-100)
+cam_rgb.still.link(video_enc.input)
+
+xout_rgb = pipeline.create(dai.node.XLinkOut)
+xout_rgb.setStreamName("frame")
+video_enc.bitstream.link(xout_rgb.input)  # HQ frames (MJPEG encoded bitstream)
+
+# Create XLinkIn node to send control commands to color camera node
+xin_ctrl = pipeline.create(dai.node.XLinkIn)
+xin_ctrl.setStreamName("control")
+xin_ctrl.out.link(cam_rgb.inputControl)
+
+# Define control command to capture still frame
+ctrl_capture = dai.CameraControl().setCaptureStill(True)
+
 # Connect to OAK device and start pipeline in USB2 mode
 with dai.Device(pipeline, maxUsbSpeed=dai.UsbSpeed.HIGH) as device:
-
-    logging.info("Recording time: %s min\n", int(REC_TIME / 60))
-
-    # Get free disk space (MB)
-    disk_free = round(psutil.disk_usage("/").free / 1048576)
 
     # Create output queue(s) to get the frames from the output(s) defined above
     q_frame = device.getOutputQueue(name="frame", maxSize=4, blocking=False)
     if args.save_lq_frames:
         q_frame_lq = device.getOutputQueue(name="frame_lq", maxSize=4, blocking=False)
 
+    # Create input queue to send control commands to OAK camera
+    q_ctrl = device.getInputQueue(name="control", maxSize=4, blocking=False)
+
+    # Wait for 3 seconds to let camera adjust auto focus and exposure
+    time.sleep(3)
+
     # Set start time of recording
     start_time = time.monotonic()
+    logging.info("Recording time: %s min\n", int(REC_TIME / 60))
 
     # Record until recording time is finished
     # Stop recording early if free disk space drops below threshold
     while time.monotonic() < start_time + REC_TIME and disk_free > MIN_DISKSPACE:
 
-        # Update free disk space (MB)
-        disk_free = round(psutil.disk_usage("/").free / 1048576)
+        # Send control command to OAK camera to capture still frame
+        q_ctrl.send(ctrl_capture)
 
-        # Get HQ (+ LQ) frames and save to .jpg
         if q_frame.has():
-            frame_hq = q_frame.get().getCvFrame()
+            # Get MJPEG encoded HQ frame and save to .jpg
             timestamp_frame = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
-            path_hq = f"{save_path}/{timestamp_frame}.jpg"
-            cv2.imwrite(path_hq, frame_hq)
+            frame_hq = q_frame.get().getData()
+            with open(save_path / f"{timestamp_frame}.jpg", "wb") as jpg:
+                jpg.write(frame_hq)
 
-        if args.save_lq_frames:
-            if q_frame_lq.has():
+            if args.save_lq_frames and q_frame_lq.has():
+                # Get LQ frame and save to .jpg
                 frame_lq = q_frame_lq.get().getCvFrame()
-                path_lq = f"{save_path}/LQ_frames/{timestamp_frame}_LQ.jpg"
-                cv2.imwrite(path_lq, frame_lq)
+                cv2.imwrite(f"{save_path}/LQ_frames/{timestamp_frame}_LQ.jpg", frame_lq)
 
-        # Wait for specified amount of seconds (default: 0.8 for 1080p; 0.2 for 4K)
-        time.sleep(CAPTURE_FREQ)
+            # Update free disk space (MB)
+            disk_free = round(psutil.disk_usage("/").free / 1048576)
+
+            # Wait for specified amount of seconds (default: 1)
+            time.sleep(CAPTURE_FREQ)
 
 # Print number and directory of saved frames
 num_frames_hq = len(list(save_path.glob("*.jpg")))
